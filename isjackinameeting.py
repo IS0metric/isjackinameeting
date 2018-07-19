506c7e9a6da7dd26dcab294f73172777c3aef04d
IS0metric <swgbhero@ymail.com>
2018-07-19 23:33:51 +0100

    Revamp with Exchange

--- isjackinameeting.py
+++ isjackinameeting.py
@@ -1,28 +1,36 @@
 # Flask app
-from __future__ import print_function
 from flask import Flask, render_template
+import datetime
+from pyexchange import Exchange2010Service, ExchangeNTLMAuthConnection
+from secret import SECRET_URL, SECRET_USERNAME, SECRET_PASSWORD
+
 app = Flask(__name__)
 
-import datetime
-import urllib2
-import urllib
-import json
-from icalendar import Calendar
-import os
-
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
-def getCalendar():
-    with open(os.path.join(__location__, 'secret.json')) as data_file:
-        data = json.load(data_file)
-    response = urllib.request.urlopen(data["cal_url"]) # CHANGE FOR URLLIB
-    calendar = response.read()
-    return Calendar.from_ical(calendar)
+
+def connect(time):
+    connection = ExchangeNTLMAuthConnection(url=SECRET_URL,
+                                            username=SECRET_USERNAME,
+                                            password=SECRET_PASSWORD)
+    service = Exchange2010Service(connection)
+    my_calendar = service.calendar()
+    events = my_calendar.list_events(
+        start=datetime.datetime(time.date().year, time.date().month, time.date().day, time.time().hour, time.time().minute, 0),
+        end=datetime.datetime(time.date().year, time.date().month, time.date().day, time.time().hour, time.time().minute, 0),
+    )
+    if events.events == []:
+        return "no"
+    oof = False
+    for event in events.events:
+        print(event.subject)
+        if event.subject.lower() == "out of office":
+            return "ooF"
+    return "yes"
+
 
 @app.route('/')
 def home():
-    calendar = getCalendar()
     now = datetime.datetime.now()
     nighttime = now.replace(hour=23, minute=0, second=0, microsecond=0)
     morningtime = now.replace(hour=7, minute=0, second=0, microsecond=0)
@@ -31,22 +39,21 @@ def home():
         "mainString": "NO",
         "subString": "He's free right now. You can get in touch."
     }
-
     if now > nighttime or now < morningtime:
-        context["mainString"] = "NO..."
-        context["subString"] = "...but it IS his night time, so he might not be awake."
+        context["mainString"] = "NO"
+        context["subString"] = "But it IS his night time, so he might not be awake."
+    elif now.weekday() > 4:
+        context["mainString"] = "NO"
+        context["subString"] = "But it's a weekend, so he might not be available."
     else:
-        for component in calendar.walk():
-            if component.name == "VEVENT":
-                start = component.get("dtstart").dt.replace(tzinfo=None)
-                end = component.get("dtend").dt.replace(tzinfo=None)
-                if now > start and now < end:
-                    context["mainString"] = "YES"
-                    context["subString"] = "You could try and get in touch and he will get back to you. See contact links below."
-                    if (end-now).total_seconds() / 60 < 15:
-                        context["subString"] = "But it's nearly over! You could try and get in touch and he will get back to you. See contact links below."
-                    break
+        busy = connect(now)
+        if busy == "yes":
+            context["mainString"] = "YES"
+            context["subString"] = "But if you drop him an email, he'll get back to you as soon as he can."
+        elif busy == "oof":
+            context["mainString"] = "NO"
+            context["subString"] = "But he's out of office, so might not be available"
     return render_template('home.html', context=context)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
