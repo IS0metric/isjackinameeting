9d12d56b0304ea1ddcbfb8f1a3176a1f8023ae4c
IS0metric <swgbhero@ymail.com>
2019-02-03 00:39:24 +0000

    Updated to use DB

--- isjackinameeting.py
+++ isjackinameeting.py
@@ -1,12 +1,32 @@
 # Flask app
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 import datetime
 import ssl
-from pyexchange import Exchange2010Service, ExchangeNTLMAuthConnection
 from random import randint
-from secret import SECRET_URL, SECRET_USERNAME, SECRET_PASSWORD
+import secret
 
 app = Flask(__name__)
+
+db_string = 'mysql://' + secret.DB_USERNAME + ':' + secret.DB_PASSWORD + '@' + secret.DB_HOST + '/' + secret.DB_NAME
+app.config['SQLALCHEMY_DATABASE_URI'] = db_string
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['APPLICAITON_ROOT'] = '/'
+app.config['WTF_CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = "secret"
+db = SQLAlchemy(app)
+
+class CheckIn(db.Model):
+    __tablename__ = 'in_table'
+
+    name = db.Column(db.String(225), primary_key=True)
+    inmeeting = db.Column(db.Integer)
+
+    def result(self):
+        if self.inmeeting == 1:
+            return "busy"
+        else:
+            return "free"
 
 
 def random_icon():
@@ -27,72 +47,48 @@ def random_icon():
     x = randint(0, len(icons)-1)
     return icons[x]
 
-"""
-As of 10/10/18, mailbox connection is not working. Will revisit
-"""
-def connect(time):
-    connection = ExchangeNTLMAuthConnection(url=SECRET_URL,
-                                            username=SECRET_USERNAME,
-                                            password=SECRET_PASSWORD)
-    service = Exchange2010Service(connection)
-    my_calendar = service.calendar()
-    print(my_calendar)
-    events = my_calendar.list_events(
-        start=datetime.datetime(time.date().year, time.date().month, time.date().day, time.time().hour, time.time().minute, 0),
-        end=datetime.datetime(time.date().year, time.date().month, time.date().day, time.time().hour, time.time().minute+1, 0)
-    )
-    if events.events == []:
-        return "no"
-    oof = False
-    for event in events.events:
-        print(event.subject)
-        if event.subject.lower() == "out of office":
-            return "ooF"
-    return "yes"
 
-
-def quickfix(time):
-    if time.isoweekday() in range(1,5) and time.hour in range(10,11) and time.minute <= 30:
-        return "yes"
-    if time.isoweekday() == 1 and time.hour in range(13,14):
-        return "yes"
-    if time.isoweekday() == 2 and time.hour in range(11, 13):
-        return "yes"
-    if time.isoweekday() == 3 and time.hour in range(16, 17):
-        return "yes"
-    if time.isoweekday() == 4 and time.hour in range(12, 14):
-        return "yes"
+def main_times(time):
+    if time.isoweekday() in range(6,8):
+        return "weekend"
     if time.isoweekday() == 5:
-        return "oof"
-    return "no"
+        return "day_off"
+    if time.hour >= 17 and time.hour < 22:
+        return "evening"
+    if time.hour >= 22 and time.hour < 5:
+        return "night"
+    if time.hour >= 5 and time.hour < 9:
+        return "morning"
+    return None
 
+
+_responses_ = {
+    "weekend": {"mainString":"No","subString":"But it's a weekend, so he might not be available."},
+    "free":{"mainString": "No","subString": "He's free right now. You can get in touch."},
+    "busy": {"mainString":"Yes","subString":"But if you drop him an email, he'll get back to you as soon as he can."},
+    "night": {"mainString": "No","subString": "But it IS his night time, so he might not be awake."},
+    "evening": {"mainString": "No","subString": "But it's outside of his usual working hours, so he might not be available"},
+    "morning":{"mainString": "No","subString": "But it's early, so he might not be in the office yet."},
+    "day_off": {"mainString": "No","subString": "But it's his day off so he might not be reachable"},
+}
+
+# PROCESS:
+#  check if weekend
+#  check if day_off
+#  check if evening_time, night_time, then morning_time
+#  check if busy (database)
+#  must be free
 
 @app.route('/')
 def home():
     now = datetime.datetime.now()
-    nighttime = now.replace(hour=23, minute=0, second=0, microsecond=0)
-    morningtime = now.replace(hour=7, minute=0, second=0, microsecond=0)
-
-    context = {
-        "mainString": "No",
-        "subString": "He's free right now. You can get in touch."
-    }
-    context["icon"] = random_icon()
-    if now > nighttime or now < morningtime:
-        context["mainString"] = "No"
-        context["subString"] = "But it IS his night time, so he might not be awake."
-    elif now.weekday() > 4:
-        context["mainString"] = "No"
-        context["subString"] = "But it's a weekend, so he might not be available."
+    meeting = main_times(now)
+    if meeting:
+        context = _responses_[meeting]
     else:
-        #busy = connect(now)
-        busy = quickfix(now) #dirtyhack
-        if busy == "yes":
-            context["mainString"] = "Yes"
-            context["subString"] = "But if you drop him an email, he'll get back to you as soon as he can."
-        elif busy == "oof":
-            context["mainString"] = "No"
-            context["subString"] = "But he's out of office, so might not be available"
+        meeting = CheckIn.query.filter_by(name="main").first()
+        context = _responses_[meeting.result()]
+    context["icon"] = random_icon()
     return render_template('home.html', context=context)
 
 if __name__ == "__main__":
